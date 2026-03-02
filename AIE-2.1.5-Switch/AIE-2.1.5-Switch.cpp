2692f2b8ba6c47ec6bd1daf6e3425c8e9b4971f9
s251388 <s251388@students.aie.edu.au>
2026-03-03 01:31:34 +1100

    Tested each potion
    Used asserts to test the ApplyPotion function with each potion type, and added a function to run all the asserts one-by-one because keeping all 4 stat checks in one looked way too messy.

--- AIE-2.1.5-Switch/AIE-2.1.5-Switch.cpp
+++ AIE-2.1.5-Switch/AIE-2.1.5-Switch.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <cassert>
 
 using namespace std;
 
@@ -46,10 +47,24 @@ Player ApplyPotion(Player NewStats, PotionType Type) {
 	return NewStats;
 }
 
+void AssertAll(Player Compare, int A_Health, int A_Speed, int A_Strength, int A_Magic) {
+
+	assert(Compare.Health == A_Health);
+	assert(Compare.Speed == A_Speed);								// Running asserts with 4 really long &&'d together arguments in each felt too messy, instead i
+	assert(Compare.Strength == A_Strength);							// opted to make a function that runs all the asserts so i can use just a small function call.
+	assert(Compare.Magic == A_Magic);
+
+}
+
 
 int main()
 {
     
+	const Player Test;
 
+	AssertAll(ApplyPotion(Test, HEALTH), 100, 10, 15, 30);
+	AssertAll(ApplyPotion(Test, SPEED), 20, 50, 5, 10);
+	AssertAll(ApplyPotion(Test, STRENGTH), 60, 25, 200, 30);
+	AssertAll(ApplyPotion(Test, MAGIC), 30, 10, 10, 100);
 
 }
